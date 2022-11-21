a7ff8ae91c4f79d8c375ddfbfd2298ec9be36273
Mathias Wagner <germannewsmaker@gmail.com>
2022-11-21 18:17:10 +0100

    Moved the configuration loader to a separate function

--- src/de/tutorialwork/main/Main.java
+++ src/de/tutorialwork/main/Main.java
@@ -12,13 +12,12 @@ import java.io.IOException;
 
 public class Main extends JavaPlugin {
 
-    static File file = new File("plugins//Adventskalender//data.yml");
-    static YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+    private static final File dataFile = new File("plugins//Adventskalender//data.yml");
+    private static final YamlConfiguration dataConfig = YamlConfiguration.loadConfiguration(dataFile);
 
-    static File config = new File("plugins//Adventskalender//config.yml");
-    static YamlConfiguration configcfg = YamlConfiguration.loadConfiguration(config);
+    private static final File configFile = new File("plugins//Adventskalender//config.yml");
 
-    public static String Prefix = "§cAdventskalender §8| §7";
+    public static String Prefix;
 
     @Override
     public void onEnable() {
@@ -28,56 +27,23 @@ public class Main extends JavaPlugin {
         if(!folder.exists()){
             folder.mkdir();
         }
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        if(!config.exists()){
-            try {
-                config.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            Prefix = configcfg.getString("Prefix").replace("&", "§");
-        }
-        configcfg.options().copyDefaults(true);
-        configcfg.addDefault("Prefix", "&cAdventskalender &8| &7");
-        configcfg.addDefault("RewardMSG", "&aDu erhälst deine Belohnung aus dem %day%. Türchen");
-        configcfg.addDefault("AlreadyEarned", "&cDu hast das heutige Türchen bereits geöffnet");
-        configcfg.addDefault("NotCurrentDay", "&cDieser Tag ist heute nicht");
-        configcfg.addDefault("Reward.1", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.2", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.3", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.4", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.5", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.6", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.7", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.8", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.9", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.10", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.11", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.12", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.13", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.14", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.15", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.16", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.17", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.18", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.19", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.20", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.21", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.22", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.23", "give %player% minecraft:diamond 1");
-        configcfg.addDefault("Reward.24", "give %player% minecraft:diamond 1");
+
         try {
-            configcfg.save(config);
-        } catch (IOException e) {
+            loadConfiguration();
+            Prefix = getConfig().getString("Prefix").replace("&", "§");
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Loads the plugin & data configuration
+     */
+    public void loadConfiguration() throws IOException {
+        if (!configFile.exists())
+            saveDefaultConfig();
+        if (!dataFile.exists())
+            dataFile.createNewFile();
     }
 
     public static ItemStack createItem(Material material, int anzahl, int subid, String displayname)
@@ -92,17 +58,17 @@ public class Main extends JavaPlugin {
     }
 
     public static void setUsed(String UUID, int Day){
-        cfg.set(UUID+".day"+Day, true);
+        dataConfig.set(UUID+".day"+Day, true);
         try {
-            cfg.save(file);
+            dataConfig.save(dataFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static boolean hasUsed(String UUID, int Day){
-        if(cfg.getString(UUID+".day"+Day) != null){
-            if(cfg.getBoolean(UUID+".day"+Day) == true){
+        if(dataConfig.getString(UUID+".day"+Day) != null){
+            if(dataConfig.getBoolean(UUID+".day"+Day) == true){
                 return true;
             } else {
                 return false;
